2f2bae24c699a142f11891fd5f1943045017bdbb
TommiMueller <92796408+TommiMueller@users.noreply.github.com>
2022-03-28 17:27:45 +0200

    Update Test_Monet.py

--- Test_Monet.py
+++ Test_Monet.py
@@ -1,39 +1,45 @@
-# Imports
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-
-# Google in Chrome öffnen
 browser = webdriver.Chrome(r'C:\Users\Thoma\Downloads\chromedriver_win32\chromedriver.exe')
 browser.get('https://google.com')
 browser.maximize_window()
+browser.implicitly_wait(5)
 
-# Cookies akzeptieren
+
 cookies_button = browser.find_element_by_xpath('//*[@id="L2AGLb"]/div')
 cookies_button.click()
 
 
+names = ['Amazon',
+         'Asos',
+         'H&M',
+         'MediaMarkt',
+         'Saturn',
+         'Zalando',
+         'ZARA',
+         'Ansons']
 
-# Suchleiste
-searchbar = browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-searchbar.click()
-searchbar.send_keys('Amazon Ratenzahlung', Keys.ENTER)
+for name in names:
+    print(name)
+    searchbar = browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+    searchbar.click()
+    searchbar.send_keys(name +
+                        ' Ratenzahlung',
+                        Keys.ENTER)
+    
+    first_result = browser.find_element_by_class_name("DKV0Md")
+    print(first_result.text)
+    first_result.click()
+    print(browser.current_url)
+  
+    browser.back()
+    browser.back()
 
-# Suchergebnis
-first_result = browser.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div[1]/div/div/div[1]/div[1]/div/a/h3')
-print(first_result.text)
-first_result.click()
-print(browser.current_url)
 
-
-# Browser schließen
 browser.quit()
 
 
 
 
-
-## Fragen:
-    #Brauche ich explicit waits?
-    #Brauche ich DataFrame?
